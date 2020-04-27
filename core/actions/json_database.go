bda31289af0f47c8e3e8a1777a266d2658f05515
DACUS1995 <studormarian@gmail.com>
2020-04-27 22:12:17 +0300

    database json work

--- core/actions/json_database.go
+++ core/actions/json_database.go
@@ -1,32 +1,80 @@
 package actions
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// TODO make database a singleton
+
 type DatabaseClient interface {
-	AddRecord()
+	AddRecord(name string, embedding []float32)
 	SearchRecordByName()
 	SearchRecordBySimilarity()
+	GetAll() []DatabaseRecord
+	Save()
 }
 
 type JSONDatabase struct {
-	savePath        string
-	numberOfRecords int
+	savePath         string
+	numberOfRecords  int
+	recordCollection []DatabaseRecord
+}
+
+type DatabaseRecord struct {
+	ID        int
+	Name      string
+	Embedding []float32
 }
 
 type JSONDatabaseClient struct {
-	database JSONDatabase
+	database *JSONDatabase
 }
 
 func NewJSONDatabaseClient(path string) DatabaseClient {
-
+	return &JSONDatabaseClient{
+		&JSONDatabase{path, 0, []DatabaseRecord{}},
+	}
 }
 
-func (client *JSONDatabaseClient) AddRecord()                {}
+func (client *JSONDatabaseClient) AddRecord(name string, embedding []float32) {
+	client.database.recordCollection = append(
+		client.database.recordCollection,
+		DatabaseRecord{
+			len(client.database.recordCollection),
+			name,
+			embedding,
+		},
+	)
+}
+
+func (client *JSONDatabaseClient) GetAll() []DatabaseRecord {
+	return client.database.recordCollection
+}
+
+func (client *JSONDatabaseClient) Save() {
+	client.database.saveDatabase()
+}
+
 func (client *JSONDatabaseClient) SearchRecordByName()       {}
 func (client *JSONDatabaseClient) SearchRecordBySimilarity() {}
 
 func (database *JSONDatabase) loadDatabase() {
+	jsonFile, err := os.Open(database.savePath + "database.json")
+	if err != nil {
+		fmt.Printf("Failed to load database records from %v", database.savePath)
+	}
+	defer jsonFile.Close()
 
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	json.Unmarshal(byteValue, &database.recordCollection)
 }
 
 func (database *JSONDatabase) saveDatabase() {
+	jsonContent, _ := json.Marshal(database.recordCollection)
 
+	ioutil.WriteFile(database.savePath+"database.json", jsonContent, 0644)
 }
---
+++ core/actions/json_database_test.go
@@ -0,0 +1,17 @@
+package actions
+
+import (
+	"testing"
+)
+
+func TestJSONDatabase(t *testing.T) {
+	databaseClient := NewJSONDatabaseClient("./")
+	databaseClient.AddRecord("Tudor", []float32{0.1, 0.2})
+
+	databaseClient.Save()
+	collection := databaseClient.GetAll()
+
+	if len(collection) != 1 {
+		t.Errorf("Expected: [%v] | Returned: [%v]", 1, len(collection))
+	}
+}
