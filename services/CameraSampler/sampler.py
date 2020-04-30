daa11a549254947a71f795de682270e433b226dd
DACUS1995 <studormarian@gmail.com>
2020-05-01 01:08:15 +0300

    added sampler using opencv2

--- services/CameraSampler/sampler.py
+++ services/CameraSampler/sampler.py
@@ -7,12 +7,28 @@ import glob
 import grpc
 import io
 import numpy as np
-from PIL import Image
+import cv2
 
 import service_pb2
 import service_pb2_grpc
 
 
 class Detector(service_pb2_grpc.CameraSampler):
+	def __init__(self):
+		super().__init__()
+		self.camera = cv2.VideoCapture(0)
+		self.width = cap.get(3)
+		self.height = cap.get(4)
+
 	def SampleImage(self, request, context):
-		pass
+		ret, frame = cam.read()
+
+
+		return service_pb2.Reply(
+			image = frame.tobytes(),
+			image_shape = [self.width, self.height]
+		)
+
+
+	def __del__(self):
+		self.camera.release()
