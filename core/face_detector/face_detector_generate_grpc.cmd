fc028a6e0dd1c1ee4031b16372750b76c48d083a
DACUS1995 <studormarian@gmail.com>
2020-05-02 22:07:45 +0300

    fixing++

--- core/face_detector/face_detector_generate_grpc.cmd
+++ core/face_detector/face_detector_generate_grpc.cmd
@@ -1,2 +1,2 @@
-protoc -I ../../services/FaceDetector/ ../../services/FaceDetector/service.proto --go_out=plugins=grpc:../../services/FaceDetector/
-mv -f ../../services/FaceDetector/service.pb.go ./service_face_detector.pb.go
+protoc -I ../../services/FaceDetector/ ../../services/FaceDetector/service_face_detector.proto --go_out=plugins=grpc:../../services/FaceDetector/
+mv -f ../../services/FaceDetector/service_face_detector.pb.go ./service_face_detector.pb.go
--- core/sampler/camera_sampler_generate_grpc.cmd
+++ core/sampler/camera_sampler_generate_grpc.cmd
@@ -1,2 +1,2 @@
-protoc -I ../../services/CameraSampler/ ../../services/CameraSampler/service.proto --go_out=plugins=grpc:../../services/CameraSampler/
-mv -f ../../services/CameraSampler/service.pb.go ./service_camera_sampler.pb.go
+protoc -I ../../services/CameraSampler/ ../../services/CameraSampler/service_camera_sampler.proto --go_out=plugins=grpc:../../services/CameraSampler/
+mv -f ../../services/CameraSampler/service_camera_sampler.pb.go ./service_camera_sampler.pb.go
