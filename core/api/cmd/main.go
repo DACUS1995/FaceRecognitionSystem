bb9a02534a3a35101bef371154e8e8d247f02cc8
DACUS1995 <studormarian@gmail.com>
2020-07-05 00:31:12 +0300

    work for api

--- core/api/cmd/main.go
+++ core/api/cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/DACUS1995/FaceRecognition/core/dbactions"
 	"github.com/gorilla/mux"
 )
 
@@ -15,19 +16,41 @@ func main() {
 }
 
 func InitServer() {
+	// TODO get a database injected
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", homeHandler)
 	router.HandleFunc("/embeddings", getAllEmbeddings)
+	router.HandleFunc("/embedding/generate", generateEmbedding).Methods("POST")
 	router.HandleFunc("/embedding", createNewEmbedding).Methods("POST")
 	http.Handle("/", router)
 
 	log.Fatal(http.ListenAndServe(":10000", router))
 }
 
+func generateEmbedding(w http.ResponseWriter, r *http.Request) {
+	image, _ := ioutil.ReadAll(r.Body)
+	// TODO: get image shape
+
+	facedetectorClient, err := facedetector.NewClient(*Config.FaceDetectionServiceAddress)
+	if err != nil {
+		log.Panicf("Failed to instantiate client: %v", err)
+	}
+
+	_, detectedFacesEmbeddings, err := facedetectorClient.DetectFaces(image, imageShape)
+	if err != nil {
+		log.Panicf("Error: %v", err)
+	}
+
+	fmt.Fprintf(w, "123123")
+	fmt.Println("Generated a new embedding")
+}
+
 func createNewEmbedding(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	json.Unmarshal(reqBody, &article)
+
+	newRecord := dbactions.DatabaseRecord{}
+	json.Unmarshal(reqBody, &newRecord)
 
 	fmt.Println("Added a new embedding")
 }
